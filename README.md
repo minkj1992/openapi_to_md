e9c83be347ea211ecb6fcfaea4dda7dcf6905c8c
minkj1992 <minkj1992@gmail.com>
2021-05-26 19:56:34 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# `OpenAPI` to `.md`
+
+
+- 기 프로젝트에서 json 생성
+```bash
+$ python manage.py generate_swagger swagger.json
+```
+
+- md 변형
+```bash
+$ npm install --save widdershins
+$ npm run convert:md
+``` 
+
+## refs
+
+- https://mermade.github.io/widdershins
---
+++ index.js
@@ -0,0 +1,39 @@
+const converter = require("widdershins");
+const fs = require("fs");
+const fileData = fs.readFileSync("swagger.json", "utf8");
+const swaggerFile = JSON.parse(fileData);
+
+let options = {}; // defaults shown
+options.codeSamples = true;
+options.httpsnippet = true;
+options.language_tabs = [
+  { http: "HTTP" },
+  { javascript: "JavaScript" },
+  { python: "Python" },
+];
+options.language_clients = [{ shell: "curl" }];
+//options.loadedFrom = sourceUrl; // only needed if input document is relative
+//options.user_templates = './user_templates';
+options.templateCallback = function (templateName, stage, data) {
+  return data;
+};
+options.theme = "darkula";
+options.search = true;
+options.sample = true; // set false by --raw
+options.discovery = false;
+options.includes = [];
+options.shallowSchemas = false;
+options.tocSummary = false;
+options.headings = 2;
+options.yaml = false;
+//options.resolve = false;
+//options.source = sourceUrl; // if resolve is true, must be set to full path or URL of the input document
+
+converter
+  .convert(swaggerFile, options)
+  .then((str) => {
+    fs.writeFileSync("swagger.md", str, "utf8");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
